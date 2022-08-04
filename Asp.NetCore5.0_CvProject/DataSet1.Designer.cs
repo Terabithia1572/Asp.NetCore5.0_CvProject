491aa4f4496b28f1bcb5daca5e15d32ef80b4d47
Terabithia1572 <legendys65@gmail.com>
2022-08-04 22:49:29 +0300

    My Hobby Page / Added

--- Asp.NetCore5.0_CvProject/DataSet1.Designer.cs
+++ Asp.NetCore5.0_CvProject/DataSet1.Designer.cs
@@ -5269,11 +5269,15 @@ namespace Asp.NetCore5._0_CvProject.DataSet1TableAdapters {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "select * from tbl_Hobilerim";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "select * from tbl_Hobilerim";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -5295,6 +5299,30 @@ namespace Asp.NetCore5._0_CvProject.DataSet1TableAdapters {
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual DataSet1.Tbl_HobilerimDataTable HobileriListele() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            DataSet1.Tbl_HobilerimDataTable dataTable = new DataSet1.Tbl_HobilerimDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillByHobiListele(DataSet1.Tbl_HobilerimDataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual DataSet1.Tbl_HobilerimDataTable HobiListele() {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
             DataSet1.Tbl_HobilerimDataTable dataTable = new DataSet1.Tbl_HobilerimDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
---
+++ Asp.NetCore5.0_CvProject/Hobilerim.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="Hobilerim.aspx.cs" Inherits="Asp.NetCore5._0_CvProject.Hobilerim" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+
+    <form id="Form1" runat="server">
+        <table class="table table-bordered">
+            <tr>
+                <th>ID</th>
+                <th>Hobilerim</th>
+                <th>İşlemler</th>
+            </tr>
+            <tbody>
+                <asp:Repeater ID="Repeater1" runat="server">
+                    <ItemTemplate>
+                        <tr>
+                            <td><%# Eval("ID") %></td>
+                            <td><%# Eval("Hobilerim") %></td>
+                            <td>
+                                <asp:HyperLink NavigateUrl='<%# "HobiSil.aspx?ID=" + Eval("ID") %>' ID="HyperLink2" runat="server" CssClass="btn btn-danger">Sil</asp:HyperLink>
+                                <asp:HyperLink NavigateUrl='<%# "HobiGuncelle.aspx?ID="+Eval("ID") %>' ID="HyperLink1" runat="server" CssClass="btn btn-primary">Güncelle</asp:HyperLink>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </ItemTemplate>
+                </asp:Repeater>
+        </table>
+        
+        <asp:Button ID="btn_Ekle" runat="server" Text="Ekle" CssClass="btn btn-success" />
+
+    </form>
+
+</asp:Content>
